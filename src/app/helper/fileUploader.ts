a303d56a2d60dea4f6a0a012c25ebe65b8c78af8
aalnoman2042 <abdullahalnoman2042@gmail.com>
2025-10-31 14:22:41 +0600

    user middleware

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -1,6 +1,6 @@
 import multer from 'multer'
 import path from 'path'
-
+import {v2 as cloudinary} from 'cloudinary'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -13,3 +13,5 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage })
+
+const uploadToCloudinary(file:  Express.Multer.File)
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,19 @@
+import z from 'zod'
+
+const createPatientValidationSchema = z.object({
+  password : z.string(),
+  patient :{
+    name: z.string({
+        error: "name is required"
+    }),
+    email: z.string({
+        error: "email is required"
+    }),
+    address: z.string().optional()
+
+  }  
+})
+
+export const UserValidation = {
+    createPatientValidationSchema
+}
